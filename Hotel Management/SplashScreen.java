d5c1ecadba3e5065955d80f5f4ebb685a6ed1ab7
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-10-06 21:29:50 +0530

    Intro of files 💥

--- Hotel Management/SplashScreen.java
+++ Hotel Management/SplashScreen.java
@@ -1,27 +1,31 @@
-import javax.swing.JWindow;
-import javax.swing.ImageIcon; 
-import java.awt.Graphics; 
-import java.awt.Image; 
-import java.awt.Toolkit; 
-public class SplashScreen extends JWindow { 
-    Image img=Toolkit.getDefaultToolkit().getImage("Hotels.png"); 
-    ImageIcon imgicon=new ImageIcon(img); 
-    public SplashScreen() { 
-        try { 
-            setSize(633,300); 
-            setLocationRelativeTo(null); 
+import javax.swing.*;
+import java.awt.*;
+
+class SplashScreen extends JWindow{
+    
+    private static final long serialVersionUID = 1L;
+    java.awt.Image img = Toolkit.getDefaultToolkit().getImage("Walkway.png");
+    ImageIcon icon = new ImageIcon(img);
+    
+    public SplashScreen(){
+        try{
+            setSize(633, 360);
+            setLocationRelativeTo(null);
             show();
-            Thread.sleep(5000); 
-            dispose(); 
-            javax.swing.JOptionPane.showMessageDialog((java.awt.Component) null,"Welcome", "Welcome Screen:", javax.swing.JOptionPane.DEFAULT_OPTION); 
-        } catch(Exception exception) { 
-            javax.swing.JOptionPane.showMessageDialog((java.awt.Component) null,"Error"+exception.getMessage(), "Error:", javax.swing.JOptionPane.DEFAULT_OPTION); 
-        } 
-    } 
-    public void paint(Graphics g) { 
-        g.drawImage(img,0,0,this); 
-    } 
-    public static void main(String[]args) { 
-        SplashScreen sp=new SplashScreen(); 
-    } 
+            Thread.sleep(5000);
+            dispose();
+        }
+        catch(Exception e){
+            javax.swing.JOptionPane.showMessageDialog((java.awt.Component) null,"Error"+e.getMessage(), "Error:", javax.swing.JOptionPane.DEFAULT_OPTION); 
+            System.out.println("! Something Went Wrong in Execution !");
+        }
+    }
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, this);
+        
+    }
+    public static void main(String[] args) {
+        SplashScreen m = new SplashScreen();
+    }
 }
