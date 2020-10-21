01e312b205375cfbb38eea37b5b31ba2e6a4817b
Aaris Kazi <40735215+Aaris-Kazi@users.noreply.github.com>
2020-10-21 11:37:55 +0530

    Final Bug Fix

--- Hotel Management/SplashScreen.java
+++ Hotel Management/SplashScreen.java
@@ -6,18 +6,17 @@ class SplashScreen extends JWindow{
     
     private static final long serialVersionUID = 1L;
     java.awt.Image img = Toolkit.getDefaultToolkit().getImage("Walkway.png");
-    ImageIcon icon = new ImageIcon(img);
     
     public SplashScreen(){
         try{
             setSize(633, 360);
             setLocationRelativeTo(null);
-            show();
+            setVisible(true);
             Thread.sleep(5000);
             dispose();
         }
         catch(Exception e){
-            javax.swing.JOptionPane.showMessageDialog((java.awt.Component) null,"Error"+e.getMessage(), "Error:", javax.swing.JOptionPane.DEFAULT_OPTION); 
+            javax.swing.JOptionPane.showMessageDialog((java.awt.Component) null,"Error"+e.getMessage(), "Error:", javax.swing.JOptionPane.ERROR_MESSAGE); 
             System.out.println("! Something Went Wrong in Execution !");
         }
     }
@@ -27,9 +26,8 @@ class SplashScreen extends JWindow{
         
     }
     public static void main(String[] args) {
-        SplashScreen m = new SplashScreen();
+        new SplashScreen();
         Index I = new Index();
         I.Front();
-        
     }
 }
